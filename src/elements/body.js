ce1c85482d751d216001f66df30d8a6d241b9809
arielnt1993 <61444560+arielnt1993@users.noreply.github.com>
2020-05-19 04:45:17 -0300

    agregado botones para sumar y restar contador

---
+++ src/elements/body.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Btn from './button'
+
+
+function Body(){
+    return <div>
+        <Btn name="sumar" />
+        <Btn name="restar"/>
+    </div>;
+}
+
+export default Body;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Body from './elements/body';
+import './css/style.css';
+
+
+ReactDOM.render(
+  <Body/>,
+  document.getElementById('root')
+);
