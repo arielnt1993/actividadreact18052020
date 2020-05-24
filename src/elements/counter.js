ced7a4594dafb2fe593198eaeafb15a3eb5450c8
arielnt1993 <61444560+arielnt1993@users.noreply.github.com>
2020-05-23 23:21:29 -0300

    correccion de error debido a mala comprencion de la actividad

--- src/elements/counter.js
+++ src/elements/counter.js
@@ -5,14 +5,15 @@ import "semantic-ui-css/semantic.min.css"
 
 const Counter = (text) =>{
     const [counter,setCounter]= useState(0);
-    const addCounter = () => setCounter(counter+1);
-    const subCounter = () => setCounter(counter-1);
+    const addCounter = () => setCounter(counter+5);
+    const subCounter = () => setCounter(counter-5);
+    /* lei mal y pense que era de uno en uno hasta 5....
     if (counter >= 6){
         alert('no podes is mas alla de 5')
         setCounter(counter-1);
     } else if (counter <= -6){
         alert('no podes ir mas alla de -5');
-        setCounter(counter+1);}
+        setCounter(counter+1);}*/
 
 
 
